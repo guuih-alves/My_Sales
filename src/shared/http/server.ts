d6b9f190fe6863847547539014c46981cbc14234
guuih_alves <guilherme_alves_08@hotmail.com>
2025-08-19 20:20:12 -0300

    feat: users migration

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'reflect-metadata'
 import routes from './routes';
 import 'express-async-errors';
+import { errors } from 'celebrate'
 import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
 import { AppDataSource } from '@shared/typeorm/data-source';
 import { error } from 'console';
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 app.use(ErrorHandleMiddleware.handleError);
 
  console.log('Connected to the Database')
