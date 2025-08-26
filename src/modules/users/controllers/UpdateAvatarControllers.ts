aa5bfe9998eaffbe2014a014888706269fc047ba
guuih_alves <guilherme_alves_08@hotmail.com>
2025-08-26 20:39:15 -0300

    feat: ignore uploads

---
+++ src/modules/users/controllers/UpdateAvatarControllers.ts
@@ -0,0 +1,15 @@
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
+import { Request, Response } from "express";
+
+export default class UpdateAvatarControllers {
+    async update(request: Request, response:Response): Promise<Response> {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            userId:Number(request.user.id),
+            avatarFileName: request.file?.filename as string,
+        });
+
+        return response.json();
+    }
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from "@modules/products/routes/ProductRoutes";
+import avatarRouter from "@modules/users/routes/AvatarRoutes";
 import sessionRouter from "@modules/users/routes/SessionRoutes";
 import usersRouter from "@modules/users/routes/UserRoutes";
 import { Router } from "express";
@@ -13,5 +14,6 @@ routes.get('/health', (request, response) => {
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter )
 routes.use('/sessions', sessionRouter)
+routes.use('/avatar', avatarRouter)
 
 export default routes;
