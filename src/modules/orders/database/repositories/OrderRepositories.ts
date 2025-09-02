f1d958e8095572add6c02916271a2e96425d28c3
guuih_alves <guilherme_alves_08@hotmail.com>
2025-09-02 19:39:59 -0300

    feat: order repositories

---
+++ src/modules/orders/database/repositories/OrderRepositories.ts
@@ -0,0 +1,33 @@
+import { AppDataSource } from "@shared/typeorm/data-source";
+import { Order } from "../entities/Orders";
+import { Customer } from "@modules/customers/database/entities/Customers";
+import { Product } from "@modules/products/database/entities/Product";
+import { OrdersProducts } from "../entities/OrdersProducts";
+
+interface ICreateOrder {
+    customer: Customer,
+    products: OrdersProducts[]
+}
+
+export const orderRepositories = AppDataSource.getRepository(Order).extend({
+    async findById(id: number): Promise<Order | null > {
+        const order = await this.findOne({
+            where: {id},
+            relations: ['order_products', 'customer'],
+        });
+
+        return order;
+    },
+
+    async createOrder({ customer, products}: ICreateOrder): Promise<Order>{
+        const order = this.create({
+            customer, 
+            order_products: products,
+        });
+
+        await this.save(order);
+
+        return order;
+
+    }
+})
